231f0bfae4a3b2c13c6b3ee94b8ea192778d81d4
Sam Eakin <sseakin@gmail.com>
2016-12-09 20:37:02 -0600

    hash table is sort of working

--- SpellCheck.java
+++ SpellCheck.java
@@ -4,44 +4,31 @@ import java.io.*;
 public class SpellCheck extends BuildDict{
 
 	public static TreeMap<String, Integer> dictionary = new TreeMap<String, Integer>();
-	public static WordList<String, String> table = new WordList<String, String>();
 
 	public static void main(String[] args){
 
-		// 1. Create Empty Hash Table to put words inside.
-		System.out.println(table.getSize());
-		System.out.println(table.isEmpty());
-
-		// 2. Read words from myDict.dat into dictionary.
+		// Read words from myDict.dat into dictionary.
 		readDictFromFile();
 
-		// 3. Hash each word
-		readDictionary();
-		
-		// 4. Place all words from dictionary into table.
-		
+		// Create table
+		WordList table = new WordList(100);
 
+		// Hash each word & place all into table
+		readDictionaryIntoTable(table);
+
+		table.find("apple");
+		table.find("biography");
+		table.find("boigraphy");
+
+		System.out.println("Number of words = "+table.getWordCount());
 	}
 
-	public static void readDictionary(){
+	public static void readDictionaryIntoTable(WordList table){
 		for(String word : dictionary.keySet()){
 			// hash word and add to table
-			String key = hash(word);
-			System.out.println(key+" = "+word);
-		}	
+			table.insertInTable(word);
+		}
 	}
-
-	public static String hash(String word){
-		if(word.length() <= 1){return word.substring(0);}
-		String key = "";
-		key = word.substring(0, 1);
-		return key;
-	}
-
-	public static void add(){
-		// @TODO: add each word to table based on hash key
-	}
-
 
 	// Read myDict.dat into dictionary
 	public static void readDictFromFile(){
@@ -60,9 +47,7 @@ public class SpellCheck extends BuildDict{
 		catch(ClassNotFoundException e){
 			e.printStackTrace();
 		}
-
 	}
-
 }
 
 
--- WordList.java
+++ WordList.java
@@ -2,46 +2,147 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
 
-// Node class to store each hash key, word, and link to another word.
-class Node<K,V>{
+/*
+ *
+ * Word List (Hash Table)
+ * Holds Buckets of Words
+ *
+ */
+public class WordList{
+	public Bucket[] theArray;
+	public int tableSize; // N
 
-	K key; // Hash key
-	V value; // Word
-	
-	Node<K,V> next; // Link to next word in chain
+	public int numberOfWords; // n
+	public float loadFactor; // N/n <= 1.5
 
 	// Constructor
-	public Node(K key, V value){
-		this.key = key;
-		this.value = value;
+	WordList(int size){
+		tableSize = size;
+		theArray = new Bucket[size];
+
+		// fill empty list
+		for(int i=0; i < tableSize; i++){
+			theArray[i] = new Bucket();
+		}
+	}
+
+	public int getWordCount(){
+		int count = 0;
+		for(int i = 0; i < tableSize; i++){
+			int wordsInCurrentBucket = theArray[i].numberOfWords;
+			count += wordsInCurrentBucket;
+		}
+		return count;
+	}
+
+	public int stringHashFunction(String word){
+		int hashKeyValue = word.hashCode() % tableSize;
+		if(hashKeyValue < 0)
+			hashKeyValue += tableSize;
+		return hashKeyValue;
+	}
+
+
+	// Insert a word into the table
+	public void insertInTable(String word){
+		Word newWord = new Word(word);
+		String wordToHash = newWord.word;
+		int hashKey = stringHashFunction(wordToHash);
+		
+		theArray[hashKey].insertInBucket(newWord, hashKey);
+	}
+
+	// Find a word in the table
+	public Word find(String searchWord){
+		int hashKey = stringHashFunction(searchWord);
+		Word theWord = theArray[hashKey].find(hashKey, searchWord);
+		return theWord;
+	}
+
+	public void displayArray(){
+		for(int i=0; i < tableSize; i++){
+			System.out.println("Array index = "+i);
+			theArray[i].printList();
+		}
+	}
+}
+
+/* Word class 
+ * Fields: hash key, String word, Word link
+ */
+class Word{
+	public String word;
+	public int key;
+	public Word next;
+
+	public Word(String word){
+		this.word = word;
+	}
+
+	public String toString(){
+		return word;
+	}
+
+	public void printWord(){
+		System.out.println(word);
 	}
 }
 
 // Container class to put nodes inside.
-public class WordList<K,V>{
+class Bucket{
 
-	private ArrayList<Node<K,V>> wordBuckets;
+	public int numberOfWords = 0;
+	public Word firstWord = null;
 
-	// capacity of array list
-	private int numberOfBuckets;
+	// Insert a word into table
+	public void insertInBucket(Word newWord, int hashKey){
+		Word previous = null;
+		Word current = firstWord;
 
-	// array list size
-	private int size;
+		newWord.key = hashKey;
 
-	public WordList(){
-		wordBuckets = new ArrayList<>();
-		numberOfBuckets = 10;
-		size = 0;
+		while(current != null && newWord.key > current.key){
+			previous = current;
+			current = current.next;
+		}
 
-		// Create empty chains
-		for(int i = 0; i < numberOfBuckets; i++){
-			wordBuckets.add(null);
+		if(previous == null)
+			firstWord = newWord;
+		else
+			previous.next = newWord;
+
+		newWord.next = current;
+		numberOfWords++;
+	}
+
+	// Find a word in the table
+	public Word find(int hashKey, String searchWord){
+		Word current = firstWord;
+		while(current != null && current.key <= hashKey){
+			if(current.word.equals(searchWord)){
+				current.printWord();
+				return current;
+		}
+			current = current.next;
+		}
+		System.out.println("Word not found.");
+		return null;
+	}
+
+	public void printList(){
+		Word current = firstWord;
+		while(current != null){
+			System.out.println(current);
+			current = current.next;
 		}
 	}
 
-	public int getSize(){return size;}
-	public boolean isEmpty(){return size == 0;}
+}//end
 
-}
+
+
+
+
+
 
 
